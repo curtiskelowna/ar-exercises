97c77237fe4214d8a420bcfb5ff59a653a7dc5bb
Curtis Platt <curtisplatt@hotmail.com>
2023-09-20 17:15:36 -0400

    exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,11 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+new_store = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+puts new_store.errors.full_messages
+new_store = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+puts new_store.errors.full_messages
+new_store = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+puts new_store.errors.full_messages
+new_store = Store.create(name: "Burnaby")
+puts new_store.errors.full_messages
